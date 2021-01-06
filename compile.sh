4ae5d0fc5221aac084b179dbb2d7afc006ae0d0a
Samyak Jain <70690987+Sammy970@users.noreply.github.com>
2021-01-06 21:25:19 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,52 +1,23 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade -y
-echo "Credits gghhkm https://github.com/gghhkm"
-Set=$1
-Sync=$2
-DeviceTrees=$3
-Build=$4
+echo " Hello, Thankyou for using this script "
+echo " You can easily build orange fox with this script for any device you like"
+echo " First lets setup the environment "
+read Ans
+_________________________________________________________________________________________
 
-#Setup
-if [ "$Set" -eq 1 ]; then
-	cd ~ || exit
-	sudo apt install git aria2 -y
-	git clone https://gitlab.com/OrangeFox/misc/scripts
-	cd scripts || exit
-	sudo bash setup/android_build_env.sh
-	sudo bash setup/install_android_sdk.sh
-elif [ "$Set" -eq 0 ]; then
-	echo "Setup Skipped"
-fi
-
-
-#Sync
-if [ "$Sync" -eq 1 ]; then
-	mkdir OrangeFox || exit
-	cd OrangeFox || exit
-	repo init --depth=1 -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
-	repo sync -j8 --force-sync
-elif [ "$Sync" -eq 0 ]; then
-	echo "Sync Skipped"
-fi
-
-#DeviceTree, Kernel, Vendor Cloning
-if [ "$DeviceTrees" -eq 0 ]; then
-	echo "Trees skipped"
-
-elif [ "$DeviceTrees" = 'jasmin_sprout' ]; then
-	cd
-        cd scripts/OrangeFox
-	git clone https://github.com/gghhkm/Xiaomi_Jasmine_Sprout_OFRP_Tree.git -b test device/xiaomi/jasmine_sprout
-        cd
-  
-#Build
-if [ "$Build" -eq 0 ]; then
-	echo "Build skipped"
-elif [ "$Build" = 'jasmine_sprout' ]; then
-	cd 
-	cd scripts/OrangeFox
-	source build/envsetup.sh
-	lunch omni_jasmine_sprout-eng && mka recoveryimage | tee XMIA2Log.txt
-fi
-echo "done"
+cd
+git clone https://github.com/akhilnarang/scripts
+bash scripts/setup/android_build_env.sh
+_________________________________________________________________________________________
+echo " Now lets sync the Latest Orange Fox Sources [ Latest 9.0 ] "
+read ans 
+cd
+cd scripts
+mkdir Orangefox
+cd Orangefox
+repo init --depth=1 -q -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
+repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+_________________________________________________________________________________________
+clear
+_________________________________________________________________________________________
+echo " Now give me what you want to export "
+read 
