1a8e1f33576a69b386205995be587fbb42aa82f3
gghhkm <72711094+gghhkm@users.noreply.github.com>
2021-01-07 03:04:55 -0500

    Update compile.sh
    very minor fixes

--- compile.sh
+++ compile.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-echo " Hello, Thankyou for using this script "
+echo " Hello, Thank you for using this script "
 echo " You can easily build orange fox with this script for your Xiaomi Device "
 echo " First lets setup the environment "
 echo " Press Enter to Start "
-read Ans
+read -r Ans
 
 echo "_________________________________________________________________________________________"
 
-cd
+cd || return
 git clone https://github.com/akhilnarang/scripts
 bash scripts/setup/android_build_env.sh
 
 echo "_________________________________________________________________________________________"
 
 echo " Now lets sync the Latest Orange Fox Sources [ Latest 9.0 ] "
-read ans 
-cd
-cd scripts
+read -r  ans
+cd || return
+cd scripts || return
 mkdir Orangefox
-cd Orangefox
+cd Orangefox || return
 repo init --depth=1 -q -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
-repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j"$(nproc --all)"
 
 echo "_________________________________________________________________________________________"
 
@@ -29,26 +29,26 @@ clear
 echo "_________________________________________________________________________________________"
 
 echo " Now tell me your Xiaomi device codename "
-read code 
+read -r code
 
 echo " Now Give me your trees github link "
-read trees
+read -r  trees
 
-cd
-cd scripts/Orangefox
-git clone $trees device/xiaomi/$code
+cd || return
+cd scripts/Orangefox || return
+git clone "$trees" device/xiaomi/"$code"
 
 echo "_________________________________________________________________________________________"
 
 echo " Now lets start building the environment "
-cd
-cd scripts/Orangefox/
+cd || return
+cd scripts/Orangefox/ || return
 source build/envsetup.sh
 
 echo "_________________________________________________________________________________________"
 
 echo " Now tell me the name of the maintainer "
-read main
+read -r main
 
 echo "_________________________________________________________________________________________"
 
@@ -68,7 +68,7 @@ export FOX_REPLACE_BUSYBOX_PS=1
 export FOX_REPLACE_TOOLBOX_GETPROP=1
 export FOX_USE_TAR_BINARY=1
 export FOX_USE_NANO_EDITOR=1
-export FOX_VERSION="R11 OP6T TEST BUILD"
+export FOX_VERSION="R11_Mi_A2_TEST_BUILD"
 export OF_USE_TWRP_SAR_DETECT=1
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
@@ -90,7 +90,7 @@ clear
 echo "_________________________________________________________________________________________"
 
 echo " Lets Lunch it all together 😉😋 "
-lunch omni_$code
+lunch omni_"$code"
 
 echo "_________________________________________________________________________________________"
 
@@ -99,6 +99,6 @@ mka recoveryimage
 
 echo "_________________________________________________________________________________________"
 
-echo " Thankyou for using my Script " 
+echo " Thank you for using my Script "
 echo " Do follow my Github Account for more scripts : https://github.com/Sammy970 "
 echo " Would love to help you "
